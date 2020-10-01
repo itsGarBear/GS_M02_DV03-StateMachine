757f85d645721e89bb6ec59632e64e0be8d514f3
itsGarBear <g.smith61900@gmail.com>
2020-10-01 14:15:34 -0700

    Add files via upload

---
+++ Assets/Scripts/StandingState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandingState : IPlayerState
+{
+    public void Enter(Player player)
+    {
+        player.mCurrentState = new StandingState();
+        Debug.Log("Entered: Standing State");
+    }
+
+    public void Execute(Player player)
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            JumpingState jumpingState = new JumpingState();
+            jumpingState.Enter(player);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            DuckingState duckingState = new DuckingState();
+            duckingState.Enter(player);
+        }
+    }
+}
